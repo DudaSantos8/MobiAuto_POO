acffa9bd5cda2b71f794350a80953dc23efb64fc
Duda Santos <maria.oliveira@zup.com.br>
2024-08-13 12:13:40 -0300

    add 2 examples about how the system work

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Motor motorCarro = new Motor(258.0, "V6 turbo diesel");
+        Carro car = new Carro("Volkswagen", "Amarok V6", 2024, 4, motorCarro);
+
+        Motor motorMoto = new Motor(14.9, "OHC, Monocilíndrico 4 tempos, arrefecido a ar,com uma cilindrada de 162,7 cc");
+        Moto moto1 = new Moto("Honda", "CG 160 Fan", 2024, "CG", motorMoto);
+
+        System.out.println(car+"\n-------------------------------\n");
+        System.out.println(moto1+"\n-------------------------------\n");
     }
 }
